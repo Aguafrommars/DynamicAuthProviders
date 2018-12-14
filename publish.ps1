34d22f81556cd2e81ccf80febf669150e32b308f
Olivier Lefebvre <aguacongas@gmail.com>
2018-12-14 20:03:47 +0100

    ci: configure appveyor

--- publish.ps1
+++ publish.ps1
@@ -1,8 +1,9 @@
-gci -Path src -rec `
-| ? { $_.Name -like "*.csproj"
+$fileversion = "$env:SemVer.0" 
+Get-ChildItem -Path src -rec `
+| Where-Object { $_.Name -like "*.csproj"
      } `
-| % { 
-    dotnet msbuild $_.FullName -t:Build -p:Configuration=Release -p:OutputPath=..\..\artifacts\build -p:GeneratePackageOnBuild=true -p:Version=$env:GitVersion_NuGetVersion -p:FileVersion=$env:GitVersion_AssemblySemVer -p:FileVersion=$env:GitVersion_AssemblySemVer
+| ForEach-Object { 
+    dotnet msbuild $_.FullName -t:Build -p:Configuration=Release -p:OutputPath=..\..\artifacts\build -p:GeneratePackageOnBuild=true -p:Version=$env:APPVEYOR_BUILD_VERSION -p:FileVersion=$fileversion -p:FileVersion=$fileversion
     if ($LASTEXITCODE -ne 0) {
             throw "build failed" + $d.FullName
     }

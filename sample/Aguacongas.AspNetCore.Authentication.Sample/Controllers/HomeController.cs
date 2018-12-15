e08a5c3b121eb6f9e2ea1844138a1151fb4872ae
Olivier Lefebvre <aguacongas@gmail.com>
2018-12-15 02:21:47 +0100

    docs: add identity to sample

--- sample/Aguacongas.AspNetCore.Authentication.Sample/Controllers/HomeController.cs
+++ sample/Aguacongas.AspNetCore.Authentication.Sample/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
 ﻿// Project: aguacongas/DymamicAuthProviders
 // Copyright (c) 2018 @Olivier Lefebvre
+using Aguacongas.AspNetCore.Authentication.Sample.Helpers;
 using Aguacongas.AspNetCore.Authentication.Sample.Models;
-using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Diagnostics;
@@ -40,18 +41,27 @@ namespace Aguacongas.AspNetCore.Authentication.Sample.Controllers
         {
             if (ModelState.IsValid)
             {
+                OAuthOptions oAuthOptions;
+                if (HandlerHelper.GetProviderName(model.HandlerType) == "Google")
+                {
+                    oAuthOptions = new GoogleOptions();
+                }
+                else
+                {
+                    oAuthOptions = new OAuthOptions();
+                }
+
+                oAuthOptions.ClientId = model.ClientId;
+                oAuthOptions.ClientSecret = model.ClientSecret;
+
                 await _manager.AddAsync(new SchemeDefinition
                 {
                     Scheme = model.Scheme,
                     DisplayName = model.DisplayName,
                     HandlerType = _manager.ManagedHandlerType.First(t => t.Name == model.HandlerType),
-                    Options = new OAuthOptions
-                    {
-                        ClientId = model.ClientId,
-                        ClientSecret = model.ClientSecret
-                    }
+                    Options = oAuthOptions
                 });
-                return Redirect("/");
+                return RedirectToAction("List");
             }
 
             return View(model);
@@ -68,6 +78,8 @@ namespace Aguacongas.AspNetCore.Authentication.Sample.Controllers
 
             var vm = new AuthenticationViewModel
             {
+                Scheme = definition.Scheme,
+                DisplayName = definition.DisplayName,
                 HandlerType = definition.HandlerType.Name
             };
 
@@ -81,13 +93,46 @@ namespace Aguacongas.AspNetCore.Authentication.Sample.Controllers
                 return Error();
             }
 
-            return View(definition);
+            return View(vm);
+        }
+
+        [HttpPost]
+        [Route("Update/{scheme}")]
+        public async Task<IActionResult> Update(AuthenticationViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var definition = await _manager.FindBySchemeAsync(model.Scheme);
+                if (definition == null)
+                {
+                    return NotFound();
+                }
+                
+                if (definition.Options is OAuthOptions oAuthOptions) // GoogleOptions is OAuthOptions
+                {
+                    oAuthOptions.ClientId = model.ClientId;
+                    oAuthOptions.ClientSecret = model.ClientSecret;                    
+                }
+
+                definition.DisplayName = model.DisplayName;
+
+                await _manager.UpdateAsync(definition);
+            }
+
+            return View(model);
         }
 
         [Route("List")]
         public IActionResult List()
         {
             return View(_manager.SchemeDefinitions.Select(s => s.Scheme));
+        }
+
+        [Route("Delete/{scheme}")]
+        public async Task<IActionResult> Delete(string scheme)
+        {
+            await _manager.RemoveAsync(scheme);
+            return RedirectToAction("List");
         }
 
         public IActionResult Privacy()
---
+++ sample/Aguacongas.AspNetCore.Authentication.Sample/Helpers/HandlerHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Aguacongas.AspNetCore.Authentication.Sample.Helpers
+{
+    public static class HandlerHelper
+    {
+        public static string GetProviderName(string handlerType)
+        {
+            if (handlerType == typeof(GoogleHandler).Name)
+            {
+                return "Google";
+            }
+            if (handlerType == typeof(OAuthHandler<OAuthOptions>).Name)
+            {
+                return "Github";
+            }
+
+            throw new InvalidOperationException("Unknow hanlder type");
+        }
+    }
+}

e76f3e924fa38fba5046f81f79b705d085bd62ed
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2018-12-21 17:21:42 +0100

    test: use test fixture

--- test/Aguacongas.AspNetCore.Authentication.Redis.Test/DynamicManagerTest.cs
+++ test/Aguacongas.AspNetCore.Authentication.Redis.Test/DynamicManagerTest.cs
@@ -1,23 +1,23 @@
 // Project: aguacongas/DymamicAuthProviders
 // Copyright (c) 2018 @Olivier Lefebvre
-using System;
 using Aguacongas.AspNetCore.Authentication.TestBase;
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Aguacongas.AspNetCore.Authentication.Redis.Test
 {
-    public class DynamicManagerTest: DynamicManagerTestBase<SchemeDefinition>
+    public class DynamicManagerTest: DynamicManagerTestBase<SchemeDefinition>, IClassFixture<TestFixture>
     {
-        public DynamicManagerTest(ITestOutputHelper output): base(output)
+        private readonly TestFixture _fixture;
+        public DynamicManagerTest(ITestOutputHelper output, TestFixture fixture): base(output)
         {
+            _fixture = fixture;
         }
 
         protected override DynamicAuthenticationBuilder AddStore(DynamicAuthenticationBuilder builder)
-        {            
-            return builder.AddRedisStore("localhost:6379");
+        {
+            return builder.AddRedisStore(provider => _fixture.Database);
         }
     }
 }
---
+++ test/Aguacongas.AspNetCore.Authentication.Redis.Test/TestFixture.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+
+namespace Aguacongas.AspNetCore.Authentication.Redis.Test
+{
+    public class TestFixture
+    {
+        public IDatabase Database { get; }
+
+        public TestFixture()
+        {
+            var options = new ConfigurationOptions();
+            options.EndPoints.Add("localhost:6379");
+            options.AllowAdmin = true;
+
+            var multiplexer = ConnectionMultiplexer.Connect(options);
+            Database = multiplexer.GetDatabase();
+            var server = multiplexer.GetServer("localhost:6379");
+            server.FlushDatabase();
+        }
+    }
+}

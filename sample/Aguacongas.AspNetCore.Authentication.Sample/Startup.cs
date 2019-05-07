d83b865e778dc8041f5f1175f9a65c58bea16106
Olivier Lefebvre <olivier.lefebvre@ne.ch>
2019-05-07 22:48:28 +0200

    build: update packages
    BREAKING CHANGES: .Net Core 3.0

--- sample/Aguacongas.AspNetCore.Authentication.Sample/Startup.cs
+++ sample/Aguacongas.AspNetCore.Authentication.Sample/Startup.cs
@@ -102,11 +102,11 @@ namespace Aguacongas.AspNetCore.Authentication.Sample
                     };
                 }); 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {

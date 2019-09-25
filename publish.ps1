cc33b0b8e98d63d94a795ed9438b28eb4f95a795
Olivier Lefebvre <aguacongas@gmail.com>
2019-09-25 17:46:16 +0200

    fix: versioning

--- publish.ps1
+++ publish.ps1
@@ -3,7 +3,7 @@ Get-ChildItem -Path src -rec `
 | Where-Object { $_.Name -like "*.csproj"
      } `
 | ForEach-Object { 
-    dotnet msbuild $_.FullName -t:Build -p:Configuration=Release -p:OutputPath=..\..\artifacts\build -p:GeneratePackageOnBuild=true -p:Version=$env:APPVEYOR_BUILD_VERSION -p:FileVersion=$fileversion -p:FileVersion=$fileversion
+    dotnet msbuild $_.FullName -t:Build -p:Configuration=Release -p:OutputPath=..\..\artifacts\build -p:GeneratePackageOnBuild=true -p:Version=$env:SemVer -p:FileVersion=$fileversion -p:FileVersion=$fileversion
     if ($LASTEXITCODE -ne 0) {
             throw "build failed" + $d.FullName
     }
